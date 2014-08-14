e59147b8ecd71ca4c8e188b7b818a8e90f81dc88
MarcusH <marcush@lovattsmedia.com>
2014-08-14 15:24:30 +1000

    more fix stuff

---
+++ dist/angular-strap-micro.tpl.js
@@ -0,0 +1,36 @@
+/**
+ * angular-strap-micro
+ * @version v2.0.4 - 2014-08-14
+ * @link http://mgcrea.github.io/angular-strap
+ * @author Marcus Hall (marcush@lovattsmedia.com)
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+(function(window, document, undefined) {
+'use strict';
+
+// Source: aside.tpl.js
+angular.module('mgcrea.ngStrap.aside').run([
+  '$templateCache',
+  function ($templateCache) {
+    $templateCache.put('aside/aside.tpl.html', '<div class="aside" tabindex="-1" role="dialog"><div class="aside-dialog"><div class="aside-content"><div class="aside-header" ng-show="title"><button type="button" class="close" ng-click="$hide()">&times;</button><h4 class="aside-title" ng-bind="title"></h4></div><div class="aside-body" ng-bind="content"></div><div class="aside-footer"><button type="button" class="btn btn-default" ng-click="$hide()">Close</button></div></div></div></div>');
+  }
+]);
+
+// Source: modal.tpl.js
+angular.module('mgcrea.ngStrap.modal').run([
+  '$templateCache',
+  function ($templateCache) {
+    $templateCache.put('modal/modal.tpl.html', '<div class="modal" tabindex="-1" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header" ng-show="title"><button type="button" class="close" ng-click="$hide()">&times;</button><h4 class="modal-title" ng-bind="title"></h4></div><div class="modal-body" ng-bind="content"></div><div class="modal-footer"><button type="button" class="btn btn-default" ng-click="$hide()">Close</button></div></div></div></div>');
+  }
+]);
+
+// Source: tab.tpl.js
+angular.module('mgcrea.ngStrap.tab').run([
+  '$templateCache',
+  function ($templateCache) {
+    $templateCache.put('tab/tab.tpl.html', '<ul class="nav nav-tabs"><li ng-repeat="pane in panes" ng-class="{active: $index == active}"><a data-toggle="tab" ng-click="setActive($index, $event)" data-index="{{$index}}" ng-bind-html="pane.title"></a></li></ul><div class="tab-content"><div ng-repeat="pane in panes" class="tab-pane" ng-class="[$index == active ? \'active\' : \'\']" ng-include="pane.template || \'$pane\'"></div></div>');
+  }
+]);
+
+
+})(window, document);
